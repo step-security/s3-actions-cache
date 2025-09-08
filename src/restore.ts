ea26ac8d90c8221b50a5b31d65e219f3433a75a2
amanstep <aman@stepsecurity.io>
2025-09-08 11:30:59 +0530

    fix: fixed validate subscription code and removed duplicate code

--- src/restore.ts
+++ src/restore.ts
@@ -15,25 +15,10 @@ import {
   setCacheSizeOutput,
   saveMatchedKey,
   getInput,
+  validateSubscription,
 } from "./utils";
-import axios, { isAxiosError } from 'axios';
 
 process.on("uncaughtException", (e) => core.info("warning: " + e.message));
-
-async function validateSubscription(): Promise<void> {
-  const API_URL = `https://agent.api.stepsecurity.io/v1/github/${process.env.GITHUB_REPOSITORY}/actions/subscription`;
-
-  try {
-    await axios.get(API_URL, { timeout: 3000 });
-  } catch (error) {
-    if (isAxiosError(error) && error.response?.status === 403) {
-      core.error('Subscription is not valid. Reach out to support@stepsecurity.io');
-      process.exit(1);
-    } else {
-      core.info('Timeout or API not reachable. Continuing to next step.');
-    }
-  }
-}
 
 async function restoreCache() {
   try {
--- src/utils.ts
+++ src/utils.ts
@@ -189,13 +189,13 @@ export function isExactKeyMatch(): boolean {
   return result;
 }
 
-async function validateSubscription(): Promise<void> {
+export async function validateSubscription(): Promise<void> {
   const API_URL = `https://agent.api.stepsecurity.io/v1/github/${process.env.GITHUB_REPOSITORY}/actions/subscription`;
 
   try {
     await axios.get(API_URL, { timeout: 3000 });
   } catch (error) {
-    if (isAxiosError(error) && error.response) {
+    if (isAxiosError(error) && error.response?.status === 403) {
       core.error('Subscription is not valid. Reach out to support@stepsecurity.io');
       process.exit(1);
     } else {
